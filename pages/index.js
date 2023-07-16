d66294d2eca461fda963789aebf5a4d79b7fa76c
Cyddharth-Gupta <cyddharthgupta@gmail.com>
2023-07-16 18:18:01 +0530

    Optimise: added preload feature and sizes attribute for Image Component

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,8 @@ export default function Home() {
         <Layout className='pt-0'>
           <div className='flex items-center justify-between w-full'>
             <div className='w-1/2'>
-              <Image src={profilePic} alt="Siddharth's profile pic" className='ml-16'/>
+              <Image src={profilePic} alt="Siddharth's profile pic" className='ml-16' priority
+              sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw'/>
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText className='!text-6xl !text-left' text="Turning Vision Into Reality With Code And Design."/>
