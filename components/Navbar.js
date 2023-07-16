00408ac3242e81f91d3fe55ccdb760b562b36677
Cyddharth-Gupta <cyddharthgupta@gmail.com>
2023-07-16 18:38:49 +0530

    Add: built basic structure of hamburger menu and animation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
 import {DribbbleIcon, GithubIcon, LinkedinIcon, MoonIcon, PinterestIcon, SunIcon, TwitterIcon} from './Icons'
@@ -21,10 +21,23 @@ const CustomLink = ({href, title, className=''}) => {
 const Navbar = () => {
 
   const [mode, setMode] = useThemeSwitcher();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () =>{
+    setIsOpen(!isOpen);
+  }
   return (
 <header
       className="w-full px-32 py-8 font-medium flex items-center justify-between relative z-10 dark:text-light"
-      >        <nav>
+      >
+
+        <button className='flex-col items-center justify-center hidden lg:flex' onClick={handleClick}>
+          <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+          <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 
+          ${isOpen ? 'opacity-0' : 'opacity-100' }`}></span>
+          <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
+        </button>
+            
+        <nav>
             <CustomLink href={'/'} title={'Home'} className={'mr-4'}/>
             <CustomLink href={'/about'} title={'About'} className={'mx-4'}/>
             {/* <CustomLink href={'/projects'} title={"Projects"} className={'mx-4'}/>
