b25b64c2638c8256004b3cca46113927a450fce5
Cyddharth-Gupta <cyddharthgupta@gmail.com>
2023-07-30 22:13:13 +0530

    Add: lg text class in animated text in about page

--- pages/about.js
+++ pages/about.js
@@ -44,7 +44,7 @@ const about = () => {
         <TransitionEffect/>
         <main className='w-full flex flex-col items-center justify-center dark:text-light'>
             <Layout className='pt-16'>
-                <AnimatedText text={'Passion Fuels Purpose!'} className='mb-16'/>
+                <AnimatedText text={'Passion Fuels Purpose!'} className='mb-16 lg:!text-7xl'/>
                 <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
                     <div className='col-span-3 flex flex-col items-start justify-start'>
 
